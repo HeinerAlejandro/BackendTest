61c235ffe3cef0043a1f6826930deeeb7fed8bb4
HeinerAlejandro <heiner.enis@gmail.com>
2021-12-11 11:37:24 -0400

    feature: Add Poll Model

--- polls/models.py
+++ polls/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Poll(models.Model):
+    class AgeTypes(models.Choices):
+        FROM_18_25 = '18-25'
+        FROM_26_33 = '26-33'
+        FROM_34_40 = '34-40'
+        MORE_40 = '40+'
+
+    class SocialNetworkTypes(models.Choices):
+        FACEBOOK = 'facebook'
+        WHATSAPP = 'whatsapp'
+        TWITTER = 'twitter'
+        INSTAGRAM = 'instagram'
+        TIKTOK = 'tiktok'
+
+    email = models.EmailField('email', unique=True)
+    age = models.CharField('age', max_length=5, choices=AgeTypes.choices)
+
+    favorite_social_network = models.CharField(max_length=9, choices=SocialNetworkTypes.choices)
+
+    time_facebook_avg = models.DecimalField('time facebook avg', max_digits=4, decimal_places=2)
+    time_whatsapp_avg = models.DecimalField('time whatsapp avg', max_digits=4, decimal_places=2)
+    time_twitter_avg = models.DecimalField('time twitter avg', max_digits=4, decimal_places=2)
+    time_instagram_avg = models.DecimalField('time instagram avg', max_digits=4, decimal_places=2)
+    time_tiktok_avg = models.DecimalField('time tiktok avg', max_digits=4, decimal_places=2)
