478cef3db5b8a6b5b0093d77c09a3be2c8ed9938
HeinerAlejandro <heiner.enis@gmail.com>
2021-12-12 19:22:25 -0400

    feature: Add gender field into Poll Model

--- polls/models.py
+++ polls/models.py
@@ -17,9 +17,13 @@ class Poll(models.Model):
         INSTAGRAM = 'instagram'
         TIKTOK = 'tiktok'
 
+    class GenderTypes(models.Choices):
+        MALE = "male"
+        FEMALE = "female"
+
     email = models.EmailField('email', unique=True)
     age = models.CharField('age', max_length=5, choices=AgeTypes.choices)
-
+    gender = models.CharField('gender', max_length=9, choices=GenderTypes.choices)
     favorite_social_network = models.CharField(max_length=9, choices=SocialNetworkTypes.choices)
 
     time_facebook_avg = models.DecimalField('time facebook avg', max_digits=4, decimal_places=2)
