187b609ae49817982f65dd71aa63def010db5555
HeinerAlejandro <heiner.enis@gmail.com>
2021-12-13 14:57:20 -0400

    feature: Add MostUsedPerAgeSerializer

--- polls/serializers.py
+++ polls/serializers.py
@@ -7,3 +7,11 @@ class PollSerializer(serializers.ModelSerializer):
     class Meta:
         model = Poll
         fields = '__all__'
+
+
+class MostUsedPerAgeSerializer(serializers.Serializer):
+    facebook = serializers.CharField()
+    whatsapp = serializers.CharField()
+    instagram = serializers.CharField()
+    twitter = serializers.CharField()
+    tiktok = serializers.CharField()
