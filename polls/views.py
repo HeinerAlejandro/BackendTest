1d4f167f35948aca6e738b18e2041e7c26f2140f
HeinerAlejandro <heiner.enis@gmail.com>
2021-12-13 14:58:08 -0400

    feature: Add most_used_sn_per_age extra action

--- polls/views.py
+++ polls/views.py
@@ -1,10 +1,14 @@
-from django.shortcuts import render
+import operator
+
+from django.db.models import Avg, Count
 
 from rest_framework import viewsets
 from rest_framework.viewsets import mixins
+from rest_framework.decorators import action
+from rest_framework.response import Response
 
 from .models import Poll
-from .serializers import PollSerializer
+from .serializers import PollSerializer, MostUsedPerAgeSerializer
 
 # Create your views here.
 
@@ -15,5 +19,21 @@ class PollAPIView(mixins.CreateModelMixin,
 
     queryset = Poll.objects.all()
     serializer_class = PollSerializer
+
+    @action(detail=False, methods=['get',], url_path='most-used-per-age', serializer_class=MostUsedPerAgeSerializer)
+    def most_used_sn_per_age(self, request):
+        """Return Most Used Social Networks per Age Range"""
+
+        serializer_class = self.get_serializer_class()
+
+        serializer = serializer_class({
+            'facebook': Poll.objects.age_most_uses_sn(Poll.SocialNetworkTypes.FACEBOOK),
+            'whatsapp': Poll.objects.age_most_uses_sn(Poll.SocialNetworkTypes.WHATSAPP),
+            'twitter': Poll.objects.age_most_uses_sn(Poll.SocialNetworkTypes.TWITTER),
+            'instagram': Poll.objects.age_most_uses_sn(Poll.SocialNetworkTypes.INSTAGRAM),
+            'tiktok': Poll.objects.age_most_uses_sn(Poll.SocialNetworkTypes.TIKTOK)
+        })
+
+        return Response(serializer.data)
 
 
